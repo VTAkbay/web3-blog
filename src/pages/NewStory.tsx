517fe912924f76d1b05e75214c887ce97a79429b
VTAkbay <vahidturabakbay1@gmail.com>
2022-07-31 17:02:32 +0300

    feat: add textfield for NewStory page

--- src/pages/NewStory.tsx
+++ src/pages/NewStory.tsx
@@ -1,5 +1,49 @@
+import { Box, Button, Container, TextField } from "@mui/material";
+
 function NewStory() {
-  return <div>New Story page</div>;
+  return (
+    <Box
+      component="main"
+      sx={{
+        alignItems: "center",
+        display: "flex",
+        flexGrow: 1,
+        minHeight: "100%",
+      }}
+    >
+      <Container
+        component="form"
+        sx={{
+          alignItems: "center",
+          display: "flex",
+          flexDirection: "column",
+          marginTop: "3rem",
+        }}
+        autoComplete="off"
+        noValidate
+        maxWidth="md"
+      >
+        <TextField
+          label="Title"
+          maxRows={1}
+          sx={{ mb: 2, width: "70%" }}
+          variant="standard"
+        />
+
+        <TextField
+          label="Tell your story..."
+          multiline
+          rows={10}
+          sx={{ width: "90%" }}
+          variant="standard"
+        />
+
+        <Button sx={{ mt: 3, width: "15ch" }} variant="outlined">
+          Publish
+        </Button>
+      </Container>
+    </Box>
+  );
 }
 
 export default NewStory;
