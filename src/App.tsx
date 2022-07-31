2f1823a2d978040f569906ea15a74e4baaee289b
VTAkbay <vahidturabakbay1@gmail.com>
2022-07-31 17:04:51 +0300

    refactor: nested routes

--- src/App.tsx
+++ src/App.tsx
@@ -11,39 +11,19 @@ import { ColorModeContext } from "./components/ToggleColorMode";
 import "@rainbow-me/rainbowkit/dist/index.css";
 import {
   darkTheme,
-  getDefaultWallets,
   lightTheme,
   RainbowKitProvider,
 } from "@rainbow-me/rainbowkit";
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
-
-const alchemyId = process.env.POLYGON_MUMBAI_ALCHEMY;
+import { chain } from "wagmi";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import NewStory from "./pages/NewStory";
+import Stories from "./pages/Stories";
+import MyStories from "./pages/MyStories";
+import NotFound from "./pages/404";
+import Home from "./pages/Home";
+import { chains } from "./lib/wagmi";
 
 export default function App() {
-  const { chains, provider } = configureChains(
-    [
-      chain.mainnet,
-      chain.polygon,
-      chain.optimism,
-      chain.arbitrum,
-      chain.polygonMumbai,
-    ],
-    [alchemyProvider({ alchemyId: alchemyId } as any), publicProvider()]
-  );
-
-  const { connectors } = getDefaultWallets({
-    appName: "Web3 Blog",
-    chains,
-  });
-
-  const wagmiClient = createClient({
-    autoConnect: true,
-    connectors,
-    provider,
-  });
-
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
 
   const [mode, setMode] = React.useState<"light" | "dark">(
@@ -70,7 +50,7 @@ export default function App() {
   );
 
   return (
-    <WagmiConfig client={wagmiClient}>
+    <HashRouter>
       <RainbowKitProvider
         coolMode
         theme={theme.palette.mode === "dark" ? darkTheme() : lightTheme()}
@@ -81,10 +61,18 @@ export default function App() {
         <ColorModeContext.Provider value={colorMode}>
           <ThemeProvider theme={theme}>
             <CssBaseline />
-            <Header></Header>
+            <Header />
+
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="create-story" element={<NewStory />} />
+              <Route path="stories" element={<Stories />} />
+              <Route path="my-stories" element={<MyStories />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
           </ThemeProvider>
         </ColorModeContext.Provider>
       </RainbowKitProvider>
-    </WagmiConfig>
+    </HashRouter>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,65 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter, Route, Routes } from "react-router-dom";
-import NewStory from "./pages/NewStory";
-import Stories from "./pages/Stories";
-import MyStories from "./pages/MyStories";
-import NotFound from "./pages/404";
+import { WagmiConfig } from "wagmi";
+import { wagmiClient } from "./lib/wagmi";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <React.StrictMode>
-              <App />
-            </React.StrictMode>
-          }
-        />
-        <Route
-          path="create-story"
-          element={
-            <React.StrictMode>
-              <App />
-              <NewStory></NewStory>
-            </React.StrictMode>
-          }
-        />
-        <Route
-          path="stories"
-          element={
-            <React.StrictMode>
-              <App />
-              <Stories></Stories>
-            </React.StrictMode>
-          }
-        />
-        <Route
-          path="my-stories"
-          element={
-            <React.StrictMode>
-              <App />
-              <MyStories></MyStories>
-            </React.StrictMode>
-          }
-        />
-        <Route
-          path="*"
-          element={
-            <React.StrictMode>
-              <App />
-              <NotFound></NotFound>
-            </React.StrictMode>
-          }
-        />
-      </Routes>
-    </HashRouter>
+    <WagmiConfig client={wagmiClient}>
+      <App />
+    </WagmiConfig>
   </React.StrictMode>
 );
 
