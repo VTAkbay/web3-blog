ae5e82e8354e7888082bb1ec53348eae8789b05c
VTAkbay <vahidturabakbay1@gmail.com>
2022-08-13 20:20:11 +0300

    feat: update contractAddress & make mockImg func

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,4 +1,5 @@
 import ABI from "../blog-abi.json";
+import axios from "axios";
 
 export const alchemyHTTPS = process.env.REACT_APP_ALCHEMY_HTTPS;
 
@@ -7,8 +8,14 @@ export const defaultNftMetadataImage =
 
 export const contractInterface = ABI;
 
-export const contractAdress = "0xc771E7DafBB9596eE8f9c92643F6947E0FA3353a";
+export const contractAdress = "0x85eE0Db07D8774b59637a52ea85200df82638F7A";
 
 export const openInNewTab = (url: string) => {
   window.open(url, "_blank", "noopener,noreferrer");
 };
+
+export async function mockImg() {
+  return await axios.get("https://picsum.photos/1920/1080").then((res) => {
+    return res.request.responseURL;
+  });
+}
